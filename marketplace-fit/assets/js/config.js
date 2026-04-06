de47cbe0bba157dcdc32562a7f52d683b4e833ce
eSellerFit <giorgi.chakhidze@gmail.com>
2026-04-06 13:49:11 -0400

    Update config.js

--- marketplace-fit/assets/js/config.js
+++ marketplace-fit/assets/js/config.js
@@ -1,5 +1,5 @@
 window.ESF_CONFIG = {
-  webAppUrl: 'https://hook.us2.make.com/nlz437jbe9lx9f2llujukmpnpdgtyoqv',
+  webAppUrl: 'https://hook.us2.make.com/z6pkiqhkxbo07j3dov3rxagnivk6egam',
   bookingUrl: '../booking/free-call.html',
   freeCallUrl: '../booking/free-call.html',
   paidBookingUrl: '../booking/paid.html'
